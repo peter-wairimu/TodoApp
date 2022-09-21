4ad85b941e99b88010bcf881ce665ed4022c02fe
peter-wairimu <peter.wairimu@student.moringaschool.com>
2022-09-21 09:09:47 +0300

    update filter buttons

--- src/App.js
+++ src/App.js
@@ -6,17 +6,39 @@ import React, {useState} from 'react';
 import { nanoid} from "nanoid"
 
 
+const FILTER_MAP = {
+  All: () => true,
+  Active: (task) => !task.completed,
+  Completed: (task) => task.completed
 
+};
 
 function App(props) {
 
 const [tasks, setTasks] = useState(props.tasks); 
+const [filter, setFilter] = useState("All")
+
 
  
 function addTask(name) {
     const newTask = {id: `todo-${nanoid}`, name, completed: false};
     setTasks([...tasks, newTask])
   }
+
+  const FILTER_NAMES = 
+  Object.keys(FILTER_MAP);
+
+const editTask = (id, newName) => {
+  const editedTaskList = tasks.map((task) => {
+    if (id === task.id){
+      return {...task, name: newName}
+    }
+    return task;
+
+  })
+  setTasks(editedTaskList);
+
+}
 
 
 const toggleTaskCompleted = (id) => {
@@ -45,6 +67,7 @@ const taskList = tasks.map((task) => (
       key={task.id}
       toggleTaskCompleted={toggleTaskCompleted}
       deleteTask = {deleteTask}
+      editTask = {editTask}
   />
 ));
 
@@ -54,7 +77,15 @@ const tasksNoun = taskList.length !== 1 ? 'tasks' : 'task';
 
 const headingText = `${taskList.length} ${tasksNoun} remaining`
 
+const filterList = FILTER_NAMES.map((name) => (
+  <FilterButton 
+  key={name} 
+  name={name}
+  isPressed={name === filter}
+  setFilter={setFilter}
 
+  />
+))
 
 
   return (
@@ -63,9 +94,8 @@ const headingText = `${taskList.length} ${tasksNoun} remaining`
       <Form addTask={addTask}/>
 
       <div className="filters btn-group stack-exception">
-        <FilterButton />
-        <FilterButton />
-        <FilterButton />
+        {filterList}
+
 
       </div>
       <h2 id="list-heading">
